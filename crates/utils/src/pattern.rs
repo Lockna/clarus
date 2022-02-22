27fa7ed1b2930b307d6abd8638a340b4d9d6b566
Raphael O <raphael.ob@protonmail.com>
2022-02-22 11:53:26 +0100

    utils: Change find_signature_ptr so easier usage for pattern searching

--- crates/utils/src/pattern.rs
+++ crates/utils/src/pattern.rs
@@ -1,19 +1,14 @@
 pub use std::io;
 pub use crate::errors;
 
-pub fn find_signature_ptr(data: &Vec<u8>, pattern: &str) -> io::Result<*const u8> {
-        let pattern_elements: Vec<_> = pattern
-            .split(' ')
-            .map(|e| u8::from_str_radix(e, 16).ok())
-            .collect();
-
-        data.windows(pattern_elements.len())
+pub fn find_signature_ptr(data: &[u8], pattern: &[u8]) -> io::Result<*const u8> {
+        data.windows(pattern.len())
             .find(|window| {
-                pattern_elements
+                pattern
                     .iter()
                     .zip(window.iter())
                     .all(|(pattern_byte, mem_byte)| {
-                        pattern_byte.map_or(false, |pred| pred == *mem_byte)
+                        *pattern_byte == *mem_byte
                     })
             })
             .map(|window| window.as_ptr())
@@ -27,7 +22,7 @@ pub fn find_signature_ptr(data: &Vec<u8>, pattern: &str) -> io::Result<*const u8
 
 
 // Dirty hack, find better solution for index finding
-pub fn find_signature_index(data: &Vec<u8>, pattern: &str) -> Result<usize, errors::Error> {
+pub fn find_signature_index(data: &Vec<u8>, pattern: &[u8]) -> Result<usize, errors::Error> {
         
         let ptr = find_signature_ptr(&data, &pattern);
 
