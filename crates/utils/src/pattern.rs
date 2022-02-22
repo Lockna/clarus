3a2dfcaf88f8c0969f326719add3fd289364ddcb
Raphael O <raphael.ob@protonmail.com>
2022-02-22 12:06:26 +0100

    utils: Rewrite find_signature_index function

--- crates/utils/src/pattern.rs
+++ crates/utils/src/pattern.rs
@@ -1,32 +1,15 @@
 pub use std::io;
-pub use crate::errors;
 
-pub fn find_signature_ptr(data: &[u8], pattern: &[u8]) -> io::Result<*const u8> {
-        data.windows(pattern.len())
-            .find(|window| {
-                pattern
-                    .iter()
-                    .zip(window.iter())
-                    .all(|(pattern_byte, mem_byte)| {
-                        *pattern_byte == *mem_byte
-                    })
-            })
-            .map(|window| window.as_ptr())
-            .ok_or_else(|| {
+pub fn find_signature_index(data: &[u8], pattern: &[u8]) -> io::Result<usize> {
+        data.windows(pattern.len()).position(|window| {
+            pattern
+                .iter()
+                .zip(window.iter())
+                .all(|(pattern_byte, mem_byte)| *pattern_byte == *mem_byte)
+        }).ok_or_else(|| {
                 io::Error::new(
                     io::ErrorKind::UnexpectedEof,
                     "failed to find signature pattern in memory",
                 )
-            })
-}
-
-
-// Dirty hack, find better solution for index finding
-pub fn find_signature_index(data: &Vec<u8>, pattern: &[u8]) -> Result<usize, errors::Error> {
-        
-        let ptr = find_signature_ptr(&data, &pattern);
-
-        let index = ptr.unwrap() as usize - data.as_ptr() as usize;
-
-        Ok(index)
+        })
 }
