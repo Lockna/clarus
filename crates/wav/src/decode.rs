d40a449257bff3fc4a4648a1d7927d446e4463a9
Raphael O <raphael.ob@protonmail.com>
2022-02-23 16:43:45 +0100

    wav: Implement own reader with functions for easier reading from file

--- crates/wav/src/decode.rs
+++ crates/wav/src/decode.rs
@@ -1,8 +1,4 @@
-use byteorder::ByteOrder;
-use byteorder::LittleEndian;
 use clarus_utils::errors::Error;
-use clarus_utils::pattern;
-use std::str;
 use crate::read::WavReader;
 use std::path::Path;
 
@@ -19,7 +15,7 @@ pub struct WavDecoder {
 
 impl WavDecoder {
 
-    pub fn new(path: &Path) -> WavDecoder {
+    pub fn new(path: &Path) -> Self {
         WavDecoder {
             reader: WavReader::new(path),
             format: 0,
@@ -29,85 +25,88 @@ impl WavDecoder {
         }
     }
 
-    pub fn decode(&self) -> Result<(u32, Vec<i16>), Error> {
+    pub fn decode(&mut self) -> Result<(u32, Vec<i16>), Error> {
         // Files from qobuz use id3v2
 
-        let contents = &self.reader.data;
-
-        let fmt_start = pattern::find_signature_index(&contents, b"fmt ").unwrap();
+        //let fmt_start = pattern::find_signature_index(&contents, b"fmt ").unwrap();
     
-        //println!("{:?}", fmt_start);
-    
-        let riff_str = str::from_utf8(&contents[0..4]).unwrap();
+        let riff_str = self.reader.read_str();
     
         if riff_str != "RIFF" {
             return Err(Error::InvalidWAVFile);
         }
     
-        //println!("{}", riff_str);
+        println!("{}", riff_str);
     
-        let chunk_size = LittleEndian::read_u32(&contents[4..8]);
+        let chunk_size = self.reader.read_u32_le();
     
-        if chunk_size != contents.len() as u32 - 8 {
-            return Err(Error::InvalidWAVFile);
+        if chunk_size != self.reader.size() as u32 - 8 {
+           return Err(Error::InvalidWAVFile);
         }
     
-        println!("{}", contents.len());
-    
+        println!("{}", self.reader.size());
+
         println!("{}", chunk_size);
     
-        let wave_str = str::from_utf8(&contents[8..12]).unwrap();
+        let wave_str = self.reader.read_str();
     
         if wave_str != "WAVE" {
-            return Err(Error::InvalidWAVFile);
+           return Err(Error::InvalidWAVFile);
         }
     
-        //println!("{}", wave_str);
+        println!("{}", wave_str);
     
-        let fmt_str = str::from_utf8(&contents[fmt_start..fmt_start + 4]).unwrap();
+        self.reader.seek_to_pattern(b"fmt ");
+
+        let fmt_str = self.reader.read_str();
     
-        //println!("{}", fmt_str);
+        println!("{}", fmt_str);
     
-        let fmt_size = LittleEndian::read_u32(&contents[fmt_start + 4..fmt_start + 8]);
+        let fmt_size = self.reader.read_u32_le();
     
-        //println!("fmt_size: {}", fmt_size);
+        println!("fmt_size: {}", fmt_size);
     
-        let fmt_format_code = LittleEndian::read_u16(&contents[fmt_start + 8..fmt_start + 10]);
+        let fmt_format_code = self.reader.read_u16_le();
     
         println!("fmt_format_code: {}", fmt_format_code);
+
+        self.format = fmt_format_code;
     
-        let fmt_num_channels = LittleEndian::read_u16(&contents[fmt_start + 10..fmt_start + 12]);
+        let fmt_num_channels = self.reader.read_u16_le();
     
         println!("fmt_num_channels: {}", fmt_num_channels);
+
+        self.channels = fmt_num_channels;
     
-        let fmt_sample_rate = LittleEndian::read_u32(&contents[fmt_start + 12..fmt_start + 16]);
+        let fmt_sample_rate = self.reader.read_u32_le();
     
         println!("fmt_sample_rate: {}", fmt_sample_rate);
+
+        self.sample_rate = fmt_sample_rate;
     
-        let fmt_byte_rate = LittleEndian::read_u32(&contents[fmt_start + 16..fmt_start + 20]);
+        let fmt_byte_rate = self.reader.read_u32_le();
     
         println!("fmt_byte_rate: {}", fmt_byte_rate);
     
-        let fmt_block_align = LittleEndian::read_u16(&contents[fmt_start + 20..fmt_start + 22]);
+        let fmt_block_align = self.reader.read_u16_le();
     
         println!("fmt_block_align: {}", fmt_block_align);
     
-        let fmt_bits_sample = LittleEndian::read_u16(&contents[fmt_start + 22..fmt_start + 24]);
+        let fmt_bits_sample = self.reader.read_u16_le();
     
         println!("fmt_bits_sample: {}", fmt_bits_sample);
+
+        self.bitdepth = fmt_bits_sample;
     
-        // TODO: search properly for data begin
-        let data_chunk_begin = fmt_start + 24;
+        self.reader.seek_to_pattern(b"data");
     
-        let data_str = str::from_utf8(&contents[data_chunk_begin..data_chunk_begin + 4]).unwrap();
+        let data_str = self.reader.read_str();
     
         println!("{}", data_str);
     
-        let data_size = LittleEndian::read_u32(&contents[data_chunk_begin + 4..data_chunk_begin + 8]);
+        let data_size = self.reader.read_u32_le();
     
         println!("data_size: {}", data_size);
-    
-        let data_begin = data_chunk_begin + 8;
     
         let samples = data_size / fmt_num_channels as u32 / (fmt_bits_sample as u32 / 8);
     
@@ -115,18 +114,20 @@ impl WavDecoder {
     
         println!("length of song: {} seconds", samples / fmt_sample_rate);
     
-        let mut channel_data: Vec<i16> = Vec::with_capacity(data_size as usize);
-    
-        println!("{}", data_begin);
+        let mut channel_data: Vec<i16> = Vec::with_capacity(data_size as usize / 2 as usize);
+
         println!("{}", data_size);
     
         for i in (0..data_size).step_by((fmt_bits_sample / 8) as usize) {
-            let sample_value =
-                LittleEndian::read_i16(&contents[data_begin + i as usize..2 + data_begin + i as usize]);
+            let sample_value = self.reader.read_i16_le();
     
             channel_data.push(sample_value);
         }
+
+        println!("{}", channel_data.len());
+
         Ok((samples / fmt_sample_rate, channel_data))
+
     }
 
 }
--- crates/wav/src/read.rs
+++ crates/wav/src/read.rs
@@ -1,5 +1,9 @@
 use clarus_utils::file;
 use std::path::Path;
+use byteorder::ByteOrder;
+use byteorder::LittleEndian;
+use clarus_utils::pattern;
+use std::str;
 
 pub struct WavReader {
 
@@ -10,14 +14,68 @@ pub struct WavReader {
 
 // Implement functions like read_u16_le (little endian or big endian)
 // seek and so on
+// set to position in data vector
 
 impl WavReader {
 
-    pub fn new(path: &Path) -> WavReader {
+    pub fn new(path: &Path) -> Self {
         WavReader {
             data: file::read_file(path).unwrap(),
             cursor: 0
         }
+    }
+
+    pub fn seek(&mut self, index: usize) {
+        self.cursor = index;
+    }
+
+    pub fn seek_to_pattern(&mut self, pattern: &[u8]) {
+        self.cursor = pattern::find_signature_index(&self.data, pattern).unwrap();
+    }
+
+    pub fn seek_forward(&mut self, inc_by: usize) {
+        self.cursor += inc_by;
+    }
+
+    pub fn read_u16_le(&mut self) -> u16 {
+
+        let ret = LittleEndian::read_u16(&self.data[self.cursor..self.cursor+2]);
+
+        self.cursor += 2;
+
+        ret
+
+    }
+
+    pub fn read_i16_le(&mut self) -> i16 {
+
+        let ret = LittleEndian::read_i16(&self.data[self.cursor..self.cursor+2]);
+
+        self.cursor += 2;
+
+        ret
+
+    }
+
+    pub fn read_u32_le(&mut self) -> u32 {
+
+        let ret = LittleEndian::read_u32(&self.data[self.cursor..self.cursor+4]);
+
+        self.cursor += 4;
+
+        ret
+
+    }
+
+    pub fn read_str(&mut self) -> &str {
+        let ret = str::from_utf8(&self.data[self.cursor..self.cursor+4]).unwrap();
+        self.cursor += 4;
+
+        ret
+    }
+
+    pub fn size(&self) -> usize {
+        self.data.len()
     }
 
 }
