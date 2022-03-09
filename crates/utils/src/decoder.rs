ea5967531583f15d3b5c82646ea1787a48ed7f83
Raphael O <raphael.ob@protonmail.com>
2022-03-09 19:02:58 +0100

    Add FIXME comment

--- crates/utils/src/decoder.rs
+++ crates/utils/src/decoder.rs
@@ -1,6 +1,7 @@
 use crate::errors::WaveError;
 
 pub trait Decoder {
+    // FIXME: Find way to have different error enums per decoder
     fn decode(&mut self) -> Result<Vec<f32>, WaveError>;
     // TODO: Add functions like split_data_into_channels and so on, which later can be used for the playback library
 }
